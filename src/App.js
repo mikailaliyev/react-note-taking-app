cde6e1b59ce31b663c7d1c374f4176271d997036
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-24 23:43:55 +0400

    working with local storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import "./App.css";
 import { useState } from "react";
-// import * as fs from 'fs';
 
 function App() {
   const [text, setText] = useState("");
-  // const fs = require("fs");
-  // const addNote = () => {
-  //   fs.writeFile("../db/db.json", "utf8");
-  // };
+  const handleClick = () => {
+    if (!text) {
+      return;
+    }
+    localStorage.setItem(new Date().toUTCString(), text);
+    setText("");
+  };
   return (
     <div className="main-note">
       <h1>Notes</h1>
+      <h2>{localStorage.getItem("Tue, 24 May 2022 19:42:26 GMT")}</h2>
       <textarea
         value={text}
         className="note-text"
         placeholder="Enter your note here"
         onChange={(e) => setText(e.target.value)}
       ></textarea>
-      {<button className="note-add-button"></button>}
+      <button className="note-add-button" onClick={handleClick}>
+        Add note
+      </button>
     </div>
   );
 }
