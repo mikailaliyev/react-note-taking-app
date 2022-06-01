26b9904c63859fed8635a3e59a602bf8539d67ce
mikailaliyev <mikail.aliyev@gmail.com>
2022-06-01 23:03:59 +0400

    update

--- src/AddNote.js
+++ src/AddNote.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 const AddNote = () => {
+    const [text, setText] = useState("");
+    const [header, setHeader] = useState("");
+    const items = Object.keys({ ...localStorage });
+  
+    const handleClick = () => {
+      if (!text) {
+        return;
+      }
+      localStorage.setItem(header, text);
+      setText("");
+      setHeader("");
+    };
     return ( 
         <div>
-            
+              <div>
+        {" "}
+        {items.map((i) => (
+          <p key={i}>{i}</p>
+        ))}
+      </div>
+      <div className="main-note">
+        <h1>Notes</h1>
+
+        <input
+          autoFocus
+          value={header}
+          type="text"
+          className="note-header"
+          placeholder="Enter note's title"
+          onChange={(e) => setHeader(e.target.value)}
+        />
+        <textarea
+          value={text}
+          className="note-text"
+          placeholder="Enter note here"
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <button className="note-add-button" onClick={handleClick}>
+          Add note
+        </button>
+      </div>
         </div>
      );
 }
--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
 import "./App.css";
-import { useState } from "react";
+import AddNote from "./AddNote";
 
 function App() {
-  const [text, setText] = useState("");
-  const [header, setHeader] = useState("");
-  const items = Object.keys({ ...localStorage });
 
-  const handleClick = () => {
-    if (!text) {
-      return;
-    }
-    localStorage.setItem(header, text);
-    setText("");
-    setHeader("");
-  };
   return (
     <div className="container">
-      <div>
-        {" "}
-        {items.map((i) => (
-          <p key={i}>{i}</p>
-        ))}
-      </div>
-      <div className="main-note">
-        <h1>Notes</h1>
-
-        <input
-          autoFocus
-          value={header}
-          type="text"
-          className="note-header"
-          placeholder="Enter note's title"
-          onChange={(e) => setHeader(e.target.value)}
-        />
-        <textarea
-          value={text}
-          className="note-text"
-          placeholder="Enter note here"
-          onChange={(e) => setText(e.target.value)}
-        ></textarea>
-        <button className="note-add-button" onClick={handleClick}>
-          Add note
-        </button>
-      </div>
+    <AddNote />
     </div>
   );
 }
